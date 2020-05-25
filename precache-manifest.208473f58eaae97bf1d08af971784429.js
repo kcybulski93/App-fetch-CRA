241df987dc0bd2a4eaafbccd47609a2c00e2e0ad
Keyper <kcybulski93@gmail.com>
2020-05-25 20:49:41 +0200

    Updates

---
+++ precache-manifest.208473f58eaae97bf1d08af971784429.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d0e38ff3b07734979f8cc9f36e95e7",
+    "url": "/App-fetch-CRA/index.html"
+  },
+  {
+    "revision": "c685eacef2a43d79f166",
+    "url": "/App-fetch-CRA/static/css/main.00503ea1.chunk.css"
+  },
+  {
+    "revision": "63f6386db5c92e1b5d7e",
+    "url": "/App-fetch-CRA/static/js/2.d9cc7207.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/App-fetch-CRA/static/js/2.d9cc7207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c685eacef2a43d79f166",
+    "url": "/App-fetch-CRA/static/js/main.52450ce4.chunk.js"
+  },
+  {
+    "revision": "bd84528a2050671ec477",
+    "url": "/App-fetch-CRA/static/js/runtime-main.18c7f1bf.js"
+  },
+  {
+    "revision": "0db6086971614b7193d2deb74c3e8dce",
+    "url": "/App-fetch-CRA/static/media/logo.0db60869.png"
+  },
+  {
+    "revision": "23e2b6cf75076c68a788053631d1f14d",
+    "url": "/App-fetch-CRA/static/media/tlo.23e2b6cf.png"
+  }
+]);
